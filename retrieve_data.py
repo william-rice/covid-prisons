e10ada5c5a7e7b26d5d91679893aa8ed1cb2f8e5
Nick Crews <nicholas.b.crews@gmail.com>
2021-01-11 16:32:27 -0800

    update retrieve_data.py

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,23 +1,22 @@
 import requests
-import re
+import os
 
 # A simple script for downloading the data from the Marshall Project's public repo
 
-def retrieve(dataset_url):
+def retrieve(filename):
     # make request
-    resp = requests.get(dataset_url)
+    template_url="https://raw.githubusercontent.com/themarshallproject/COVID_prison_data/master/data/{}"
+    resp = requests.get(template_url.format(filename))
 
     # save file
-    filename = re.search("(?<=/data/).+", dataset_url).group()
-    with open(filename, 'wb') as f:
+    with open('folder' + os.sep + filename, 'wb') as f:
         f.write(resp.content)
 
 def main():
-    template_url="https://raw.githubusercontent.com/themarshallproject/COVID_prison_data/master/data/{}"
-    retrieve(template_url.format("covid_prison_cases.csv"))
-    retrieve(template_url.format("covid_prison_rates.csv"))
-    retrieve(template_url.format("prison_populations.csv"))
-    retrieve(template_url.format("staff_populations.csv"))
+    retrieve("covid_prison_cases.csv")
+    retrieve("covid_prison_rates.csv")
+    retrieve("prison_populations.csv")
+    retrieve("staff_populations.csv")
 
 if __name__ == "__main__":
     main()
